240795fa0f389e1b4e9661dda78e74db75c6a10a
Racyn Ibaya <racynibaya@outlook.com>
2022-06-17 10:23:20 -0400

    First commit

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './header.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="header">Header</div>;
 };
 
 export default Header;
